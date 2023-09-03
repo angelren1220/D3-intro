8dcaf09a0ed2e397c66d95d8eee6b86ff98b9e28
Linuxe <angelren1220@gmail.com>
2023-09-03 19:23:56 -0400

    add scale scatter plot

--- scatter.js
+++ scatter.js
@@ -1,19 +1,28 @@
 const scatterDataset = [
-  [ 34,    78 ],
-  [ 109,   280 ],
-  [ 310,   120 ],
-  [ 79,    411 ],
-  [ 420,   220 ],
-  [ 233,   145 ],
-  [ 333,   96 ],
-  [ 222,   333 ],
-  [ 78,    320 ],
-  [ 21,    123 ]
+  [34, 78],
+  [109, 280],
+  [310, 120],
+  [79, 411],
+  [420, 220],
+  [233, 145],
+  [333, 96],
+  [222, 333],
+  [78, 320],
+  [21, 123]
 ];
 
 
 const sw = 500;
 const sh = 500;
+const padding = 60;
+
+const xScale = d3.scaleLinear()
+  .domain([0, d3.max(scatterDataset, (d) => d[0])])
+  .range([padding, sw - padding]);
+
+const yScale = d3.scaleLinear()
+  .domain([0, d3.max(scatterDataset, (d) => d[1])])
+  .range([sh - padding, padding]);
 
 const scatterSvg = d3.select("#scatterContainer")
   .append("svg")
@@ -24,16 +33,15 @@ scatterSvg.selectAll("circle")
   .data(scatterDataset)
   .enter()
   .append("circle")
-  .attr("cx", (d) => d[0])
-  .attr("cy", (d) => sh - d[1])
-  .attr("r", 5)
+  .attr("cx", (d) => xScale(d[0]))
+  .attr("cy", (d) => yScale(d[1]))
+  .attr("r", 5);
 
 scatterSvg.selectAll(".scatterLabel")
   .data(scatterDataset)
   .enter()
   .append("text")
   .attr("class", "scatterLabel")
-// Add your code below this line
-  .text((d) => (d[0] +", "+ d[1]))
-  .attr("x", (d, i) => d[0] + 5)
-  .attr("y", (d, i) => sh - d[1])
+  .text((d) => (d[0] + ", " + d[1]))
+  .attr("x", (d, i) => xScale(d[0] + 10))
+  .attr("y", (d, i) => yScale(d[1]));
